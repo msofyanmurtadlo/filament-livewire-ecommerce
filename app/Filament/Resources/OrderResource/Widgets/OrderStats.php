98f4cc654e6635fa5186adf2407ac949238a8e6e
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-01-16 06:34:45 +0700

    Feat: Menambahkan widget dan stats di menu orders

---
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
+use Illuminate\Support\Str;
+
+class OrderStats extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('New Orders', Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
+            Stat::make('Order Shipped', Order::query()->where('status', 'shipped')->count()),
+            Stat::make(
+                'Average Price',
+                Str::of(Number::currency(Order::query()->avg('grand_total'), 'IDR', locale: 'id'))->replace(',00', '')
+            ),
+        ];
+    }
+}
