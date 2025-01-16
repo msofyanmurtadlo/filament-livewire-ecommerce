d762b25d52bdadb122310e76a3867136c04ca95a
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-01-16 23:11:26 +0700

    Feat: Menampilkan product dalam database

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class ProductDetailPage extends Component
 {
+    #[Title('Product Detail - Ecommerce')]
+
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail()
+        ]);
     }
 }
