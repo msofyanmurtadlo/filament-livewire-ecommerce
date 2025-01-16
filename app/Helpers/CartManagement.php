035401b4a8dec7cce148ecbf62a4275c635732f9
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-01-17 03:04:11 +0700

    Feat: Menambahkan fitur shopping cart

--- app/Helpers/CartManagement.php
+++ app/Helpers/CartManagement.php
@@ -38,6 +38,38 @@ class CartManagement
         return count($cart_items);
     }
 
+    // dengan quantity
+    static public function addItemToCartWithQty($product_id, $qty = 1)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+        $existing_item = null;
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                $existing_item = $key;
+                break;
+            }
+        }
+        if ($existing_item !== null) {
+            $cart_items[$existing_item]['quantity'] = $qty;
+            $cart_items[$existing_item]['total_amount'] = $cart_items[$existing_item]['quantity'] * $cart_items[$existing_item]['unit_amount'];
+        } else {
+            $product = Product::where('id', $product_id)->first(['id', 'name', 'price', 'images']);
+            if ($product) {
+                $cart_items[] = [
+                    'product_id' => $product_id,
+                    'name' => $product->name,
+                    'image' => $product->images[0],
+                    'quantity' => $qty,
+                    'unit_amount' => $product->price,
+                    'total_amount' => $product->price
+                ];
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
+        return count($cart_items);
+    }
+
     static public function removeCartItem($product_id)
     {
         $cart_items = self::getCartItemsFromCookie();
@@ -57,12 +89,12 @@ class CartManagement
 
     static public function clearCartItems()
     {
-        Cookie::queue(Cookie::forget('cart_items'));
+        Cookie::queue(Cookie::forget('cart_item'));
     }
 
     static public function getCartItemsFromCookie()
     {
-        $cart_items = json_decode(Cookie::get('cart_items'), true);
+        $cart_items = json_decode(Cookie::get('cart_item'), true);
         if (!$cart_items) {
             $cart_items = [];
         }
