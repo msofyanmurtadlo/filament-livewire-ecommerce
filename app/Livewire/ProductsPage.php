26db9bad26430a394e6b5b697f17edcca679c1df
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-01-17 00:10:22 +0700

    Feat: Menambahkan fitur filter produk

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Livewire\Attributes\Url;
 
 class ProductsPage extends Component
 {
@@ -15,9 +16,40 @@ class ProductsPage extends Component
 
     #[Title('Products - Ecommerce')]
 
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 0;
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
+        if (!empty($this->selected_categories)) {
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+        if (!empty($this->selected_brands)) {
+            $productQuery->whereIn('brand_id', $this->selected_brands);
+        }
+        if ($this->featured) {
+            $productQuery->where('is_featured', 1);
+        }
+        if ($this->on_sale) {
+            $productQuery->where('on_sale', 1);
+        }
+        if ($this->price_range) {
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+
         return view('livewire.products-page', [
             'products' => $productQuery->paginate(6),
             'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -10,8 +10,8 @@
                             @foreach ($categories as $category)
                                 <li class="mb-4" wire:key='{{ $category->id }}'>
                                     <label for="{{ $category->slug }}" class="flex items-center dark:text-gray-400 ">
-                                        <input type="checkbox" id="{{ $category->slug }}" value="{{ $category->id }}"
-                                            class="w-4 h-4 mr-2">
+                                        <input type="checkbox" wire:model.live='selected_categories'
+                                            id="{{ $category->slug }}" value="{{ $category->id }}" class="w-4 h-4 mr-2">
                                         <span class="text-lg">{{ $category->name }}</span>
                                     </label>
                                 </li>
@@ -27,8 +27,8 @@
                             @foreach ($brands as $brand)
                                 <li class="mb-4" wire:key='{{ $brand->id }}'>
                                     <label for="{{ $brand->slug }}" class="flex items-center dark:text-gray-300">
-                                        <input type="checkbox" id="{{ $brand->slug }}" value="{{ $brand->id }}"
-                                            class="w-4 h-4 mr-2">
+                                        <input type="checkbox" wire:model.live='selected_brands'
+                                            id="{{ $brand->slug }}" value="{{ $brand->id }}" class="w-4 h-4 mr-2">
                                         <span class="text-lg dark:text-gray-400">{{ $brand->name }}</span>
                                     </label>
                                 </li>
@@ -40,14 +40,16 @@
                         <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                         <ul>
                             <li class="mb-4">
-                                <label for="" class="flex items-center dark:text-gray-300">
-                                    <input type="checkbox" class="w-4 h-4 mr-2">
-                                    <span class="text-lg dark:text-gray-400">In Stock</span>
+                                <label for="featured" class="flex items-center dark:text-gray-300">
+                                    <input type="checkbox" id="featured" wire:model.live='featured' value="1"
+                                        class="w-4 h-4 mr-2">
+                                    <span class="text-lg dark:text-gray-400">Featured Products</span>
                                 </label>
                             </li>
                             <li class="mb-4">
-                                <label for="" class="flex items-center dark:text-gray-300">
-                                    <input type="checkbox" class="w-4 h-4 mr-2">
+                                <label for="on_sale" class="flex items-center dark:text-gray-300">
+                                    <input type="checkbox" id="on_sale" wire:model.live='on_sale' value="1"
+                                        class="w-4 h-4 mr-2">
                                     <span class="text-lg dark:text-gray-400">On Sale</span>
                                 </label>
                             </li>
@@ -58,12 +60,16 @@
                         <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
                         <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                         <div>
-                            <input type="range"
+                            <div class="font-semibold">
+                                {{ Str::of(Number::currency($price_range, 'IDR', 'id'))->replace(',00', '') }}</div>
+                            <input type="range" wire:model.live='price_range'
                                 class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer"
-                                max="500000" value="100000" step="100000">
+                                max="10000000" value="300000" step="10000">
                             <div class="flex justify-between ">
-                                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 1000</span>
-                                <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 500000</span>
+                                <span
+                                    class="inline-block text-lg font-bold text-blue-400 ">{{ Str::of(Number::currency(10000, 'IDR', 'id'))->replace(',00', '') }}</span>
+                                <span
+                                    class="inline-block text-lg font-bold text-blue-400 ">{{ Str::of(Number::currency(10000000, 'IDR', 'id'))->replace(',00', '') }}</span>
                             </div>
                         </div>
                     </div>
